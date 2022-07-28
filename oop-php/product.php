4132f27b58a4356347c1d3aaec0452921177af88
triggeredsoyboy <triggeredsoyboy@gmail.com>
2022-07-29 03:56:33 +0700

    first commit and test

---
+++ oop-php/product.php
@@ -0,0 +1,8 @@
+<?php
+
+class Product
+{
+}
+
+$produk1 = new Product();
+$produk2 = new Product();
---
+++ oop-php/test.php
@@ -0,0 +1,13 @@
+<?php
+
+class Test
+{
+}
+
+$a = new Test();
+$b = new Test();
+$c = new Test();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
