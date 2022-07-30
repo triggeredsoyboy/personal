911abc763ed4f9237d617afc0ae14c2b7de2b9c7
triggeredsoyboy <triggeredsoyboy@gmail.com>
2022-07-30 13:48:34 +0700

    property and method

--- oop-php/product.php
+++ oop-php/product.php
@@ -2,7 +2,38 @@
 
 class Product
 {
+    public $title = "title",
+        $author = "author",
+        $publisher = "publisher",
+        $price = 0;
+
+    public function getLabel()
+    {
+        return "$this->author, $this->publisher";
+    }
 }
 
-$produk1 = new Product();
-$produk2 = new Product();
+// $product1 = new Product();
+// $product1->title = "Laskar Pelangi";
+// var_dump($product1);
+
+// $product2 = new Product();
+// $product2->title = "Call of Duty";
+// $product2->addProperty = "Unknown";
+// var_dump($product2);
+
+$product3 = new Product();
+$product3->title = "Naruto";
+$product3->author = "Masashi Kishimoto";
+$product3->publisher = "Shonen Jump";
+$product3->price = 30000;
+
+$product4 = new Product();
+$product4->title = "Call of Duty";
+$product4->author = "Mohammad Alavi";
+$product4->publisher = "Activision";
+$product4->price = 350000;
+
+echo "Komik : " . $product3->getLabel();
+echo "<br>";
+echo "Game : " . $product4->getLabel();
